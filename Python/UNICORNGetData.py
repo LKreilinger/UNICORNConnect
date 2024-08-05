ce9e584a1d84dd1303fe407ee53bd3de3756fd02
LKreilinger <laurens.kreilinger@web.de>
2024-08-05 11:45:41 +0200

    Checked converion: updated

--- Python/UNICORNGetData.py
+++ Python/UNICORNGetData.py
@@ -1,6 +1,7 @@
 import numpy as np
 from UNICORNConnect import UNICORNDevice
 
+
 def accel_calculation(payload, index):
     accel = np.zeros(3)
     for i in range(3):
@@ -20,6 +21,7 @@ def gyro_calculation(payload, index):
         gyro[i] = value / 32.8  # convert in Angular Velocity [°/s]
     return gyro
 
+
 def UNICORNGetData(ser, samples):
     # Set up parameters
     nchan = 16
@@ -28,23 +30,23 @@ def UNICORNGetData(ser, samples):
     start_sequence = [0xC0, 0x00]
     stop_sequence = [0x0D, 0x0A]
 
-    data = np.zeros([samples, nchan])
+    data = np.zeros([samples, nchan]).astype(np.float32)
 
     for sample in range(samples):
         payload = ser.read(45)
         # to check payload conversion, results in pdf
         # % https://github.com/unicorn-bi/Unicorn-Suite-Hybrid-Black/blob/master/Unicorn%20Bluetooth%20Protocol/UnicornBluetoothProtocol.pdf
-        #payload = np.array(
+        # payload = np.array(
         #    [0xC0, 0x00, 0x0F, 0x00, 0x9F, 0xAF, 0x00, 0x9F, 0xD4, 0x00, 0xA0, 0x40, 0x00, 0x9F, 0x43, 0x00, 0x9F, 0x9A,
         #     0x00, 0x9F, 0xE3, 0x00, 0x9F, 0x85, 0x00, 0x9F, 0xBB, 0x2E, 0xF6, 0xE9, 0x02, 0x8D, 0xF2, 0xF3, 0xFF, 0xEF,
         #     0xFF, 0x23, 0x00, 0xB0, 0x00, 0x00, 0x00, 0x0D, 0x0A], dtype=np.uint8)
-        # payload = bytearray(payload)
+        payload = bytearray(payload)
         if payload[0:2] != bytearray(start_sequence):
             raise Exception("Invalid packet")
         if payload[43:45] != bytearray(stop_sequence):
             raise Exception("Invalid packet")
 
-        battery = (100 * payload[2] & 15) / 15
+        battery = 100 * (payload[2] & 15) / 15
 
         eeg = np.zeros(8)
         for ch in range(8):
